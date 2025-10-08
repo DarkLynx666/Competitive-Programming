8ac637cc0568d5332fb0bc194aa6a9c51342a8a0
DarkLynx666 <sourfist7x13@gmail.com>
2025-10-08 19:50:43 +0600

    solved problem 4 of CPS Module 1 contest

---
+++ c/contests/PRB-4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int x,y,q;
+    scanf("%d %d",&x,&y);
+
+    if((x+y)>= 0 && (x+y) <8){
+    q = 7 - (x+y);
+    }
+
+
+    printf("%d",q);
+
+    return 0;
+
+}
