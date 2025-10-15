8354c2283b48666655725818f4b56c9c48cde529
DarkLynx666 <sourfist7x13@gmail.com>
2025-10-15 13:07:25 +0600

    solved habaitta in c

---
+++ c/contests/foodCost.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+
+    int res;
+    res = x * 6;
+    int res2 = y;
+     
+    int out = res + res2;
+
+    printf("%d",out);
+
+    return 0;
+}
