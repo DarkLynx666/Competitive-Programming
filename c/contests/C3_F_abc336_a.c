7dd53bbd7375b90d411dcf704f9b033e3e4e234c
Shaan <sourfist7x13@gmail.com>
2025-12-09 20:53:42 +0600

    M-1, C-3, Problem-6, (CPS)

---
+++ c/contests/C3_F_abc336_a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    printf("L");
+    while (n>0)
+    {
+     printf("o");  
+     n--;
+    }
+    printf("ng");
+
+    return 0;
+    
+}
